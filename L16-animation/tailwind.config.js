5d977e1945ea7f87099393075b21e07d06b052f2
Karthik <56579449+karthik9728@users.noreply.github.com>
2023-09-15 14:08:18 +0530

    animation + custom animation

--- L16-animation/tailwind.config.js
+++ L16-animation/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+      Keyframes: {
+        wiggle: {
+          '0%,100%': { transform: 'rotate(-12deg)' },
+          '50%': { transform: 'rotate(12deg)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
